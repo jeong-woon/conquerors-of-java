fc0523bb99355f2d5ab326f43ea256d68c78bc4c
jeong-woon <66984173+jeong-woon@users.noreply.github.com>
2020-12-04 08:38:20 +0900

    Update jvmStudy.java
    ass comment

--- src/main/java/org/example/jvm/jvmStudy.java
+++ src/main/java/org/example/jvm/jvmStudy.java
@@ -3,5 +3,8 @@ package org.example.jvm;
 public class jvmStudy {
     public static void main(String[] args) {
         System.out.println(App.class.getSuperclass());
+        
+        // 힙영역에 객체 저장, 만들지 않아도 class 객체는 클래스 로딩하면서 만들어짐.
+        // App.class
     }
 }
