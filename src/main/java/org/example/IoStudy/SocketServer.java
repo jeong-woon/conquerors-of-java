85a7ba303c267b28771ecca2ed4e3b2490514c2e
molt <jwkoo88@gmail.com>
2021-01-23 10:53:03 +0900

    commit

---
+++ src/main/java/org/example/IoStudy/SocketServer.java
@@ -0,0 +1,47 @@
+package org.example.IoStudy;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class SocketClient {
+
+    public static void main(String[] args) {
+        SocketClient sample=new SocketClient();
+        sample.sendSocketSample();
+    }
+
+    public void sendSocketSample() {
+        for(int loop=0;loop<3;loop++) {
+            sendAndReceiveSocketData();
+        }
+    }
+
+    public void sendAndReceiveSocketData() {
+        Socket socket=null;
+        try {
+            System.out.println("Client:Connecting");
+            socket=new Socket("127.0.0.1",9999); // 1)
+            System.out.println("Client:Connect status="+socket.isConnected());
+            Thread.sleep(1000);
+            byte[] readByte=new byte[256];
+            InputStream stream=socket.getInputStream();
+            BufferedInputStream in=
+                    new BufferedInputStream(stream);
+            in.read(readByte);
+            System.out.println("Client:received data="+new String(readByte).trim());
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(socket!=null) {
+                try {
+                    socket.close(); // 3)
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/example/ListVsArrayListVsArray/ListVsArrayListVsArray.java
@@ -0,0 +1,2 @@
+package org.example.ListVsArrayListVsArray;public class ListVsArrayListVsArray {
+}
